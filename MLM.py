266fab7af8ac7af4c4a7f4137980109fbe025489
TamyhB <blenda.tamy@gmail.com>
2017-09-29 11:32:41 -0300

    Versão quase final (Ainda decidindo se vale a pena se vale a pena aplicar o unique)

--- MLM.py
+++ MLM.py
@@ -1,6 +1,12 @@
 import numpy as np
 import sklearn.datasets as database
 import math
+from sklearn.metrics.pairwise import euclidean_distances
+import random
+from sklearn.model_selection import train_test_split
+
+
+
 class MLM():
 
     def __init__(self, numero_pontos_referencia = 10):
@@ -13,6 +19,7 @@ class MLM():
         pontos_referenciaY = []
 
 
+
     def treinamento(self, X, Y):
         '''
 
@@ -20,30 +27,18 @@ class MLM():
         :param Y: matriz Y
         :return: B_hat, pontos de refencias X e Y
         '''
-
-        self.pontos_referenciaX, self.pontos_referenciaY = self.random_pontos_referencias(Y.shape[0], X, Y)
+        self.pontos_referenciaX, self.pontos_referenciaY = self.random_pontos_referencias(X.shape[0],X, Y)
+        #print self.pontos_referenciaX
+        #print X
         #matrizes de distancias
-        dX = np.zeros(shape=(self.numero_pontos_referencia, X.shape[1]))
-        dY = np.zeros(shape=(self.numero_pontos_referencia, Y.shape[1]))
-
+        dX = euclidean_distances(X, self.pontos_referenciaX)
+        dY = euclidean_distances(Y, self.pontos_referenciaY)
         #Faca a distancia dos dois pontos = raiz2((dp - dx)^2)
 
-        '''
-            for para calcular a distancia. Acho que ta errado esse for, conserto depois :v
-        '''
-        for i in range(X.shape[0]):
-            for j in range(len(self.pontos_referenciaX)-1):
-                for t in range(X.shape[1]):
-                    dX[i][j] = (math.sqrt((self.pontos_referenciaX[i][j] - X[i][j]) ** 2))
-
-        for i in range(Y.shape[0]):
-            for j in range(len(self.pontos_referenciaY)-1):
-                for t in range(X.shape[1]):
-                    dX[i][j] = (math.sqrt((self.pontos_referenciaY[i][j] - Y[i][j])**2))
-
-        B_hat = np.linalg.solve(dX,dY)
+        B_hat = np.linalg.solve(dX.T.dot(dX), dX.T).dot(dY)
         self.B_hat = B_hat
         return B_hat, self.pontos_referenciaX, self.pontos_referenciaY
+
 
 
 
@@ -55,45 +50,73 @@ class MLM():
         :return: pontos de referencias
         '''
         random_indices = []
-        random_pontosA = []
+        random_pontosX = []
         random_pontosY = []
         for i in range(self.numero_pontos_referencia):
             ponto = np.random.randint(tamanho)
-            if ponto in random_indices == False:
+            if (ponto in random_indices) == False:
                 random_indices.append(ponto)
-                random_pontosA.append(X[ponto])
+                random_pontosX.append(X[ponto])
                 random_pontosY.append(Y[ponto])
+        #print random_pontosX
+        #print "\n"
+        #print random_pontosY
+        return random_pontosX, random_pontosY
 
 
-        return random_pontosA, random_pontosY
-
-
-    def predict(self, amostraT):
+    def predict(self, amostrasTeste):
         '''
 
-        :param amostraT: amostra de treinamento, ou seja, vc manda uma amostra por vez
-        :return: predicao da amostra (Vimos o seu futuro, ele diz que vc eh Versiculor Mwahhahaha)
+        :param amostrasTeste: amostras de teste para serem classificadas
+        :return: predicao da amostras (Vimos o seu futuro, ele diz que vc eh Versicolor Mwahhahaha)
         '''
 
         #Array que vai guardar a distancia
-        DamostraT = []
 
+        dAmostraTX = euclidean_distances(amostrasTeste, self.pontos_referenciaX)
+        dY = dAmostraTX.dot(self.B_hat)
 
         '''
-        for para calcular a distancia da amostra. Acho que ta errado esse for, conserto depois :v
+        for para armazenar a classificacao da matriz de novas amostras
         '''
-        for i in range(amostraT.shape[0]):
-            for j in range(len(self.pontos_referenciaX) - 1):
-                DamostraT.append(math.sqrt((amostraT[i][j] - amostraT[i][j]) ** 2))
+        pred_hat = []
+        for indice in range(len(amostrasTeste)):
+            pred_hat.append(self.pontos_referenciaY[np.argmin(dY[indice])])
 
-        pred_hat = np.dot(amostraT, self.B_hat)
-        min_pred_hat = min(pred_hat)
-
-        predicaoIndice = np.where(self.pontos_referenciaX == min_pred_hat)[0]
-        return self.pontos_referenciaY[predicaoIndice]
+        return pred_hat
 
 
 
-#x = database.load_iris()
-#xd = x.data
-#print xd
+    def binarizar(self, y):
+        biny = np.zeros((y.shape[0], np.amax(y) + 1))
+        for i in range(len(y)):
+            biny[i][y[i]] = 1
+        return biny
+
+
+
+mlmT = MLM(40)
+iris = database.load_iris()
+X = iris.data
+y = mlmT.binarizar(iris.target)
+acertosInteracoes = []
+'''
+for i in range(20):
+    Xl, Xt, yl, yt = train_test_split(X, y, test_size=0.2)
+    mlmT.treinamento(Xl, yl)
+    predictXt = mlmT.predict(Xt)
+    acertos = 0
+    for j in range(len(yt)):
+        acertos += int(np.array_equal(yt[j],predictXt[j]))
+    acertosInteracoes.append(acertos)
+'''
+
+Xl, Xt, yl, yt = train_test_split(X, y, test_size=0.2)
+mlmT.treinamento(Xl, yl)
+predictXt = mlmT.predict(Xt)
+acertos = 0
+for j in range(len(yt)):
+    acertos += int(np.array_equal(yt[j],predictXt[j]))
+acertosInteracoes.append(acertos)
+
+print acertos
